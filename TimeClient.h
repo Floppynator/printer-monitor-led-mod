b402aa116ab946cb6fb61b6c4824f04914208158
Christian Haip <christian.haip@gmail.com>
2018-07-01 12:03:17 +0200

    Add files via upload
    Initial Commit

--- TimeClient.h
+++ TimeClient.h
@@ -1,6 +1,6 @@
-MIT License
+/**The MIT License (MIT)
 
-Copyright (c) 2018 
+Copyright (c) 2015 by Daniel Eichhorn
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,3 +19,44 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+See more at http://blog.squix.ch
+*/
+
+/*
+Modified by David Payne for use in the Scrolling Marquee
+*/
+
+#pragma once
+
+#include <ESP8266WiFi.h>
+
+#define NTP_PACKET_SIZE 48
+
+class TimeClient {
+
+  private:
+    float myUtcOffset = 0;
+    long localEpoc = 0;
+    long localMillisAtUpdate;
+    const char* ntpServerName = "www.google.com";
+    const int httpPort = 80;    
+    byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+  public:
+    TimeClient(float utcOffset);
+    void updateTime();
+    
+    void setUtcOffset(float utcOffset);
+    String getHours();
+    String getAmPmHours();
+    String getAmPm();
+    String getMinutes();
+    String getSeconds();
+    String getFormattedTime();
+    String getAmPmFormattedTime();
+    long getCurrentEpoch();
+    long getCurrentEpochWithUtcOffset();
+
+};
+
